1b0802675d3c807a90f2b2e092ef61a026513945
DESKTOP-A1R8B7A\Yw_bananapie <pavel.dinh1@gmail.com>
2018-11-12 17:35:01 +0000

    Statements and branches
    Else if:
    
    else
    {
       if(statement)
       {
       }
    }

---
+++ StatementsBranches/StatementsBranches/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Log.h"
+
+int main()
+{
+	const char* ptr = nullptr;
+	if (ptr)
+		Log(ptr);
+	else
+		Log("PTR IS NULL");
+
+	int x = 5;
+	bool comparisonResult = x == 5;
+
+	if (comparisonResult)
+	{
+		Log("Hello World");
+	}
+	std::cin.get();
+}
